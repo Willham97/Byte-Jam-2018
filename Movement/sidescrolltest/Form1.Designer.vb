af76d0ca2ac7f8f273ec7bbeed5694cb6c28ef4c
Andrew <44783277+AndrewD713@users.noreply.github.com>
2018-11-08 08:27:00 -0600

    Add files via upload

---
+++ Movement/sidescrolltest/Form1.Designer.vb
@@ -0,0 +1,151 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
+        Me.picPlayer = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.picOb1 = New System.Windows.Forms.PictureBox()
+        Me.picOb2 = New System.Windows.Forms.PictureBox()
+        Me.tmrRight = New System.Windows.Forms.Timer(Me.components)
+        Me.tmrLeft = New System.Windows.Forms.Timer(Me.components)
+        Me.tmrUp = New System.Windows.Forms.Timer(Me.components)
+        Me.tmrDown = New System.Windows.Forms.Timer(Me.components)
+        Me.picGround = New System.Windows.Forms.PictureBox()
+        Me.tmrCheck = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.picPlayer, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picOb1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picOb2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picGround, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'picPlayer
+        '
+        Me.picPlayer.Image = CType(resources.GetObject("picPlayer.Image"), System.Drawing.Image)
+        Me.picPlayer.Location = New System.Drawing.Point(324, 142)
+        Me.picPlayer.Margin = New System.Windows.Forms.Padding(4)
+        Me.picPlayer.Name = "picPlayer"
+        Me.picPlayer.Size = New System.Drawing.Size(236, 284)
+        Me.picPlayer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.picPlayer.TabIndex = 0
+        Me.picPlayer.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(4000, 129)
+        Me.PictureBox2.Margin = New System.Windows.Forms.Padding(4)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(92, 87)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = False
+        '
+        'picOb1
+        '
+        Me.picOb1.Image = CType(resources.GetObject("picOb1.Image"), System.Drawing.Image)
+        Me.picOb1.Location = New System.Drawing.Point(794, 142)
+        Me.picOb1.Margin = New System.Windows.Forms.Padding(4)
+        Me.picOb1.Name = "picOb1"
+        Me.picOb1.Size = New System.Drawing.Size(236, 284)
+        Me.picOb1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.picOb1.TabIndex = 2
+        Me.picOb1.TabStop = False
+        '
+        'picOb2
+        '
+        Me.picOb2.Image = CType(resources.GetObject("picOb2.Image"), System.Drawing.Image)
+        Me.picOb2.Location = New System.Drawing.Point(1131, 142)
+        Me.picOb2.Margin = New System.Windows.Forms.Padding(4)
+        Me.picOb2.Name = "picOb2"
+        Me.picOb2.Size = New System.Drawing.Size(236, 284)
+        Me.picOb2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.picOb2.TabIndex = 3
+        Me.picOb2.TabStop = False
+        '
+        'tmrRight
+        '
+        Me.tmrRight.Interval = 10
+        '
+        'tmrLeft
+        '
+        Me.tmrLeft.Interval = 10
+        '
+        'tmrUp
+        '
+        Me.tmrUp.Interval = 10
+        '
+        'tmrDown
+        '
+        Me.tmrDown.Interval = 10
+        '
+        'picGround
+        '
+        Me.picGround.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.picGround.Location = New System.Drawing.Point(172, 409)
+        Me.picGround.Name = "picGround"
+        Me.picGround.Size = New System.Drawing.Size(1347, 50)
+        Me.picGround.TabIndex = 4
+        Me.picGround.TabStop = False
+        '
+        'tmrCheck
+        '
+        Me.tmrCheck.Enabled = True
+        Me.tmrCheck.Interval = 1
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoScroll = True
+        Me.ClientSize = New System.Drawing.Size(1067, 554)
+        Me.Controls.Add(Me.picOb2)
+        Me.Controls.Add(Me.picOb1)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.picPlayer)
+        Me.Controls.Add(Me.picGround)
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "frmMain"
+        Me.Text = "Form1"
+        CType(Me.picPlayer, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picOb1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picOb2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picGround, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents picPlayer As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents picOb1 As PictureBox
+    Friend WithEvents picOb2 As PictureBox
+    Friend WithEvents tmrRight As Timer
+    Friend WithEvents tmrLeft As Timer
+    Friend WithEvents tmrUp As Timer
+    Friend WithEvents tmrDown As Timer
+    Friend WithEvents picGround As PictureBox
+    Friend WithEvents tmrCheck As Timer
+End Class
---
+++ Movement/sidescrolltest/Form1.vb
@@ -0,0 +1,88 @@
+﻿Public Class frmMain
+    Private faceLeft = False
+    Private faceRight = True
+    Private blnJump As Boolean = True
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.AutoScroll = False
+    End Sub
+
+    Private Sub Form1_KeyUp(sender As Object, e As KeyEventArgs) Handles Me.KeyUp
+        Select Case e.KeyCode
+            Case Keys.Right
+                tmrRight.Enabled = False
+            Case Keys.Left
+                tmrLeft.Enabled = False
+        End Select
+    End Sub
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+
+        Select Case e.KeyCode
+            Case Keys.Right
+                tmrRight.Enabled = True
+                tmrLeft.Enabled = False
+            Case Keys.Left
+                tmrRight.Enabled = False
+                tmrLeft.Enabled = True
+            Case Keys.Up
+                If blnJump = True Then
+                    tmrUp.Enabled = True
+                    blnJump = False
+                End If
+        End Select
+    End Sub
+
+    Private Sub tmrRight_Tick(sender As Object, e As EventArgs) Handles tmrRight.Tick
+        Dim Objects() As PictureBox = {picOb2, picOb1, picGround}
+        For x = 0 To Objects.Length - 1
+            Objects(x).Left -= 3
+        Next
+        'Faces the player right when moving right -AD
+        If faceRight = False Then
+            picPlayer.Image.RotateFlip(RotateFlipType.Rotate180FlipY)
+            faceRight = True
+            faceLeft = False
+            picPlayer.Refresh()
+        End If
+    End Sub
+
+    Private Sub tmrLeft_Tick(sender As Object, e As EventArgs) Handles tmrLeft.Tick
+        Dim Objects() As PictureBox = {picOb2, picOb1, picGround}
+        For x = 0 To Objects.Length - 1
+            Objects(x).Left += 3
+        Next
+        'Faces the player left when moving left -AD
+        If faceLeft = False Then
+            picPlayer.Image.RotateFlip(RotateFlipType.Rotate180FlipY)
+            faceLeft = True
+            faceRight = False
+            picPlayer.Refresh()
+        End If
+    End Sub
+
+    Private Sub tmrUp_Tick(sender As Object, e As EventArgs) Handles tmrUp.Tick
+        Static x As Integer = 0
+        If x < 20 Then
+            picPlayer.Top -= 6
+        Else
+            tmrUp.Enabled = False
+            tmrDown.Enabled = True
+            x = 0
+        End If
+        x += 1
+    End Sub
+
+    Private Sub tmrDown_Tick(sender As Object, e As EventArgs) Handles tmrDown.Tick
+        picPlayer.Top += 6
+        If picPlayer.Bounds.IntersectsWith(picGround.Bounds) AndAlso (picPlayer.Top + picPlayer.Height) < (picGround.Top + 10) Then
+            tmrDown.Enabled = False
+            blnJump = True
+        End If
+    End Sub
+
+    Private Sub tmrCheck_Tick(sender As Object, e As EventArgs) Handles tmrCheck.Tick
+        If Not picPlayer.Bounds.IntersectsWith(picGround.Bounds) And tmrUp.Enabled = False Then
+            tmrDown.Enabled = True
+        End If
+    End Sub
+End Class
